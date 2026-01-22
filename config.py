3d980b6a8b9a5c25bcb929b632d0c2a9bf7a7524
LongYkun <148337644+TZkYkunaga@users.noreply.github.com>
2026-01-23 00:42:26 +0700

    Update secret

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://postgres:postgres@localhost/smartrent_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+    #
     # Session
     PERMANENT_SESSION_LIFETIME = timedelta(hours=24)
     
